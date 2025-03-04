260ce1c4c3e2d8d7b03fd89fd1a113b7d182501d
Victoria S. Johansen <671418@stud.hvl.no>
2025-03-04 16:43:05 +0100

    Oblig ferdig

--- src/main/java/no/hvl/dat110/broker/Dispatcher.java
+++ src/main/java/no/hvl/dat110/broker/Dispatcher.java
@@ -2,8 +2,6 @@ package no.hvl.dat110.broker;
 
 import java.util.Set;
 import java.util.Collection;
-
-import no.hvl.dat110.common.TODO;
 import no.hvl.dat110.common.Logger;
 import no.hvl.dat110.common.Stopable;
 import no.hvl.dat110.messages.*;
@@ -105,58 +103,50 @@ public class Dispatcher extends Stopable {
 		storage.removeClientSession(user);
 
 	}
-
+	// Task B.2
 	public void onCreateTopic(CreateTopicMsg msg) {
-
+		
+		String topic = msg.getTopic();
 		Logger.log("onCreateTopic:" + msg.toString());
-
-		// TODO: create the topic in the broker storage
-		// the topic is contained in the create topic message
-
-		throw new UnsupportedOperationException(TODO.method());
-
+		storage.createTopic(topic);
 	}
 
 	public void onDeleteTopic(DeleteTopicMsg msg) {
 
+		String topic = msg.getTopic();
 		Logger.log("onDeleteTopic:" + msg.toString());
-
-		// TODO: delete the topic from the broker storage
-		// the topic is contained in the delete topic message
-		
-		throw new UnsupportedOperationException(TODO.method());
+		storage.deleteTopic(topic);
 	}
 
 	public void onSubscribe(SubscribeMsg msg) {
-
-		Logger.log("onSubscribe:" + msg.toString());
-
-		// TODO: subscribe user to the topic
-		// user and topic is contained in the subscribe message
 		
-		throw new UnsupportedOperationException(TODO.method());
-
+		String user = msg.getUser();
+		String topic = msg.getTopic();
+		Logger.log("onSubscribe:" + msg.toString());
+		storage.addSubscriber(user, topic);
 	}
 
 	public void onUnsubscribe(UnsubscribeMsg msg) {
-
-		Logger.log("onUnsubscribe:" + msg.toString());
-
-		// TODO: unsubscribe user to the topic
-		// user and topic is contained in the unsubscribe message
 		
-		throw new UnsupportedOperationException(TODO.method());
+		String user = msg.getUser();
+		String topic = msg.getTopic();
+		Logger.log("onUnsubscribe:" + msg.toString());
+		storage.removeSubscriber(user, topic);
 	}
 
 	public void onPublish(PublishMsg msg) {
-
-		Logger.log("onPublish:" + msg.toString());
-
-		// TODO: publish the message to clients subscribed to the topic
-		// topic and message is contained in the subscribe message
-		// messages must be sent using the corresponding client session objects
 		
-		throw new UnsupportedOperationException(TODO.method());
-
+		String topic = msg.getTopic();
+		Logger.log("onPublish:" + msg.toString());
+		Set<String> subscribers = storage.getSubscribers(topic);
+		
+		if (subscribers != null) {
+			for (String user : subscribers) {
+				ClientSession session = storage.getSession(user);
+				if (session != null) {
+					session.send(msg);
+				}
+			}
+		}
 	}
 }
--- src/main/java/no/hvl/dat110/iotsystem/TemperatureDevice.java
+++ src/main/java/no/hvl/dat110/iotsystem/TemperatureDevice.java
@@ -1,30 +1,34 @@
 package no.hvl.dat110.iotsystem;
 
 import no.hvl.dat110.client.Client;
-import no.hvl.dat110.common.TODO;
 
+// Task C
 public class TemperatureDevice {
 
 	private static final int COUNT = 10;
 
 	public static void main(String[] args) {
-
-		// simulated / virtual temperature sensor
 		TemperatureSensor sn = new TemperatureSensor();
-
-		// TODO - start
-
-		// create a client object and use it to
-
-		// - connect to the broker - user "sensor" as the user name
-		// - publish the temperature(s)
-		// - disconnect from the broker
-
-		// TODO - end
-
+		
+		Client client = new Client("sensor", Common.BROKERHOST, Common.BROKERPORT);
+		
+		client.connect();
+		
+		for (int i = 0; i < COUNT; i++) {
+			int temp = sn.read();
+			System.out.println("READING: " + temp);
+			
+			client.publish(Common.TEMPTOPIC, String.valueOf(temp));
+			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				System.err.println("Interrupted");
+			}
+		}
+		
+		client.disconnect();
 		System.out.println("Temperature device stopping ... ");
-
-		throw new UnsupportedOperationException(TODO.method());
 
 	}
 }
